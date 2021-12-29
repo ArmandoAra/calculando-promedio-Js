a347bab5f15b9dc3a51fc0e362fec41204c458eb
ArmandoAra <arliazeed@hotmail.com>
2021-12-29 16:26:02 +0100

    terminado calculo de moda en la clase, creada la funcion

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const lista1 = [
     100,
     200,
     500,
+    200,
+    200,
+    34,
+    42,
+    893,
     500000,
 ];
 
@@ -28,16 +33,39 @@ function calcularMediaAritmetica(lista){
 function calcularMediana(lista){
     var mitadLista = lista.length / 2;
 
-    if (lista.length % 2 == 0){
+    if (lista.length % 2 === 0){
         var listaMitad = [];
         listaMitad[0] = lista[mitadLista - 1];
         listaMitad[1] = lista[mitadLista];
         return calcularMediaAritmetica(listaMitad);
     }else{
-        const sortedList = lista.sort(); 
+        const sortedList = lista.sort((a, b) => a - b); 
         mitadLista = Math.floor(mitadLista);
         console.log(sortedList);     
         return sortedList[mitadLista];
     }
 }
 
+function calcularModa(lista){
+    //Crea un objeto
+    let listaCount = {};
+    // Cuenta cuanta cantidad de cada elemento hay en la lista y lo agrega al objeto
+    lista.map(
+        function (elemento){
+            if (listaCount[elemento]){
+                listaCount[elemento] += 1;
+            } else{
+            listaCount[elemento] = 1;
+            };
+        }
+    )
+    // Convierte denuevo el objeto en un array ordenandolo de menor a mayor dependiendo de la cantidad que se haya repetido
+    const lista1Array = Object.entries(listaCount).sort(
+        function (elementoA, elementoB) {
+            return elementoA[1] - elementoB[1];
+        }
+    );
+    // Escogemos el ultimo elemento del array
+    const moda = lista1Array[lista1Array.length - 1];
+    return moda;
+}
